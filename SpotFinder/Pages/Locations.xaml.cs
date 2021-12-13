54feec25bdd7e00921857e9fd75a2e02c1818b7d
MikeVerlinde <mike.verlinde@outlook.com>
2021-12-13 11:46:42 +0100

    Locations page according to new design

---
+++ SpotFinder/Pages/Locations.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotFinder.Pages
+{
+    /// <summary>
+    /// Interaction logic for Locations.xaml
+    /// </summary>
+    public partial class Locations : Page
+    {
+        public Locations()
+        {
+            InitializeComponent();
+        }
+    }
+}
