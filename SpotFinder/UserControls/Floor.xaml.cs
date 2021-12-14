977f76808d90efa47721c8577fd48376caa81c96
MikeVerlinde <mike.verlinde@outlook.com>
2021-12-14 16:01:58 +0100

    All user controls should now be ready

---
+++ SpotFinder/UserControls/Floor.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotFinder.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Floor.xaml
+    /// </summary>
+    public partial class Floor : UserControl
+    {
+
+        public Brush Color
+        {
+            set { color.Fill = value; }
+        }
+
+        public string Building
+        {
+            set { building.Text = value; }
+        }
+
+        public string Level
+        {
+            set { level.Text = value; }
+        }
+
+        public Floor()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SpotFinder/UserControls/Reservations.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotFinder.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Reservations.xaml
+    /// </summary>
+    public partial class Reservations : UserControl
+    {
+        public Brush Color
+        {
+            set { color.Fill = value; }
+        }
+        public string Building
+        {
+            set { building.Text = value; }
+        }
+
+        public string Quantity
+        {
+            set { quantity.Text = value; }
+        }
+
+        public Reservations()
+        {
+            InitializeComponent();
+        }
+    }
+}
